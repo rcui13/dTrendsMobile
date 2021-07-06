885ae5901b43323f8f484641f4697e35d8c81e3c
Ianyliu <ian.liu@g.northernacademy.org>
2021-07-06 16:37:04 -0400

    a bunch of crap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core'
 @Component({
   selector: 'ns-app',
   templateUrl: 'app.component.html',
+
 })
 export class AppComponent implements OnInit {
   constructor() {
